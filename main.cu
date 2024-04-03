32d5c6bc6349e31fffb3727914e18b105b0cafd6
Andrew Darling <andr3w.darling@gmail.com>
2024-04-02 19:15:30 -0500

    increase speed + depth with better rendering optimization and more floating point precision

--- main.cu
+++ main.cu
@@ -10,20 +10,20 @@ __device__ Uint32 getColor(int iter) {
     return (0xFF << 24) | (r << 16) | (g << 8) | b;
 }
 
-__global__ void mandelbrotSetKernel(unsigned int* output, float lowerX, float lowerY, float stepX, float stepY, int maxIter, int width, int height) {
+__global__ void mandelbrotSetKernel(unsigned int* output, double lowerX, double lowerY, double stepX, double stepY, int maxIter, int width, int height) {
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
     int idy = threadIdx.y + blockIdx.y * blockDim.y;
     if (idx >= width || idy >= height) return;
     int pixelIndex = (idy * width) + idx;
 
-    float x0 = lowerX + idx * stepX;
-    float y0 = lowerY + idy * stepY;
-    float x = 0.0;
-    float y = 0.0;
+    double x0 = lowerX + idx * stepX;
+    double y0 = lowerY + idy * stepY;
+    double x = 0.0;
+    double y = 0.0;
 
     int iteration = 0;
     while (x*x + y*y <= (2*2) && iteration < maxIter) {
-        float xtemp = x*x - y*y + x0;
+        double xtemp = x*x - y*y + x0;
         y = 2*x*y + y0;
         x = xtemp;
         iteration++;
@@ -34,11 +34,11 @@ __global__ void mandelbrotSetKernel(unsigned int* output, float lowerX, float lo
 
 
 int main() {
-    const int width = 1024, height = 768, maxIter = 1000000;
-    float lowerX = -2.5, lowerY = -1.0, upperX = 1.0, upperY = 1.0;
+    const int width = 2024, height = 1568, maxIter = 500;
+    double lowerX = -2.5, lowerY = -1.0, upperX = 1.0, upperY = 1.0;
 
-    float centerX = -1.186592f;
-    float centerY = -1.901211e-1f;
+    double centerX = -1.186592f;
+    double centerY = -1.901211e-1f;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cerr << "Could not initialize SDL: " << SDL_GetError() << std::endl;
@@ -50,6 +50,11 @@ int main() {
 
     bool quit = false;
     SDL_Event event;
+
+    unsigned int* d_output;
+    cudaMalloc(&d_output, width * height * sizeof(unsigned int));
+
+    unsigned int* pixels = new unsigned int[width * height];
 
     while (!quit) {
         float zoomFactor = 1;
@@ -64,13 +69,13 @@ int main() {
                 int mouseX, mouseY;
                 SDL_GetMouseState(&mouseX, &mouseY);
 
-                centerX = lowerX + (mouseX / (float)width) * (upperX - lowerX);
-                centerY = lowerY + (mouseY / (float)height) * (upperY - lowerY);
+                centerX = lowerX + (mouseX / (double)width) * (upperX - lowerX);
+                centerY = lowerY + (mouseY / (double)height) * (upperY - lowerY);
             }
         }
 
-        float rangeX = (upperX - lowerX) * zoomFactor;
-        float rangeY = (upperY - lowerY) * zoomFactor;
+        double rangeX = (upperX - lowerX) * zoomFactor;
+        double rangeY = (upperY - lowerY) * zoomFactor;
 
         lowerX = centerX - rangeX / 2;
         upperX = centerX + rangeX / 2;
@@ -80,13 +85,13 @@ int main() {
         float stepX = (upperX - lowerX) / width;
         float stepY = (upperY - lowerY) / height;
 
-        unsigned int* d_output;
-        cudaMalloc(&d_output, width * height * sizeof(unsigned int));
         dim3 blocks(16, 16);
         dim3 grid((width + blocks.x - 1) / blocks.x, (height + blocks.y - 1) / blocks.y);
-        mandelbrotSetKernel<<<grid, blocks>>>(d_output, lowerX, lowerY, stepX, stepY, maxIter, width, height);
 
-        unsigned int* pixels = new unsigned int[width * height];
+        double range = upperX - lowerX;
+        int dynamicMaxIter = static_cast<int>(maxIter * std::max(0.1, 1 - log(range) / log(10000)));
+
+        mandelbrotSetKernel<<<grid, blocks>>>(d_output, lowerX, lowerY, stepX, stepY, dynamicMaxIter, width, height);
         cudaMemcpy(pixels, d_output, width * height * sizeof(unsigned int), cudaMemcpyDeviceToHost);
 
         SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0, 0, 0));
@@ -97,12 +102,14 @@ int main() {
         }
 
         SDL_UpdateWindowSurface(window);
-
-        cudaFree(d_output);
-        delete[] pixels;
     }
 
     SDL_Quit();
+
+    cudaFree(d_output);
+    delete d_output;
+
+    delete pixels;
 
     return 0;
 }
